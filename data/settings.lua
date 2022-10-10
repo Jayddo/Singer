74bbd6606fc9ce8b6fce30486ad77739da6802c5
Michele <mbs@goteki.net>
2022-10-11 00:15:43 +0200

    settings

---
+++ data/settings.lua
@@ -0,0 +1,20 @@
+return {
+    ["dummy"] = L{"Puppet's Operetta","Scop's Operetta",},
+    ["debuffs"] = L{"Carnage Elegy","Pining Nocturne",},
+    ["playlist"] = T{
+        ["odyssey"] = L{"Knight's Minne V","Victory March","Valor Minuet V",},
+        ["playlist"] = L{"Blade Madrigal","Victory March","Valor Minuet V",},
+        ["list"] = L{"Blade Madrigal","Victory March","Valor Minuet V",},
+        ["melee"] = L{"Honor March","Blade Madrigal","Valor Minuet V",},
+        ["clear"] = L{},
+    },
+    ["buffs"] = T{
+        ["haste"] = L{},
+        ["refresh"] = L{},
+        ["aurorastorm"] = L{},
+        ["firestorm"] = L{},
+    },
+    ["song"] = T{
+    },
+    ["songs"] = L{"Honor March","Blade Madrigal","Valor Minuet V",},
+}
